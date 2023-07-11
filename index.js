fe87cc6918d44c914c866fcc1d998061214e3205
ian kibet <iankibet87@gmail.com>
2023-07-11 14:06:15 +0800

    algorithms palindrome

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(string) {
+  // Convert the string to lowercase
+  string = string.toLowerCase();
+
+  // Remove any non-alphabetic characters from the string
+  string = string.replace(/[^a-z]/g, '');
+
+  // Check if the reversed string is equal to the original string
+  return string === string.split('').reverse().join('');
 }
 
-/* 
-  Add your pseudocode here
-*/
-
 /*
-  Add written explanation of your solution here
+  Pseudocode:
+  1. Convert the string to lowercase.
+  2. Remove any non-alphabetic characters from the string using a regular expression.
+  3. Check if the reversed string is equal to the original string.
+  4. Return true if the string is a palindrome, false otherwise.
+
+  Explanation:
+  - The function first converts the string to lowercase to ensure case-insensitive palindrome comparison.
+  - It then removes any non-alphabetic characters from the string using a regular expression (/[^a-z]/g).
+  - The string is converted into an array of characters using the split('') method.
+  - The array is reversed using the reverse() method.
+  - The reversed array is joined back into a string using the join('') method.
+  - Finally, the function checks if the reversed string is equal to the original string and returns the result.
 */
 
 // You can run `node index.js` to view these console logs
